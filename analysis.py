46e54b268aae67246490a70e675e5d5f86e8bc58
poojajogdandaids2024-web <pooja.jogdand.aids.2024@vpkbiet.org>
2026-01-14 10:40:24 +0530

    add basic analysis module with data loading function

---
+++ analysis.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+import numpy as np
+
+def load_data(filepath):
+    """Load data from CSV file."""
+    return pd.read_csv(filepath)
+def basic_statistics(data):
+    """Calculate basic statistics."""
+    return data.describe()
+if __name__ == "__main__"
+    print("Data Science Project - Analysis Module")
